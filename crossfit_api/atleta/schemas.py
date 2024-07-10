296ad2c2c8d5a5cebc8cd452e388383675351a59
gemaquejr <gemaquejr@hotmail.com>
2024-07-09 23:49:54 -0300

    Fix: schemas.py de atleta e categorias
    Signed-off-by: gemaquejr <gemaquejr@hotmail.com>

--- crossfit_api/atleta/schemas.py
+++ crossfit_api/atleta/schemas.py
@@ -5,9 +5,9 @@ from crossfit_api.contrib.schemas import BaseSchema
 
 
 class Atleta(BaseSchema):
-    nome: Annotated[str, Field(description="Nome do atleta", examples="João", max_length=50)]
-    cpf: Annotated[str, Field(description="CPF do atleta", examples="12345678910", max_length=11)]
-    idade: Annotated[int, Field(description="Idade do atleta", examples=25)]
-    peso: Annotated[PositiveFloat, Field(description="Peso do atleta", examples=75.5)]
-    altura: Annotated[PositiveFloat, Field(description="Altura do atleta", examples=1.75)]
-    sexo: Annotated[str, Field(description="Sexo do atleta", examples="M", max_length=1)]
+    nome: Annotated[str, Field(description="Nome do atleta", example="João", max_length=50)]
+    cpf: Annotated[str, Field(description="CPF do atleta", example="12345678910", max_length=11)]
+    idade: Annotated[int, Field(description="Idade do atleta", example=25)]
+    peso: Annotated[PositiveFloat, Field(description="Peso do atleta", example=75.5)]
+    altura: Annotated[PositiveFloat, Field(description="Altura do atleta", example=1.75)]
+    sexo: Annotated[str, Field(description="Sexo do atleta", example="M", max_length=1)]
