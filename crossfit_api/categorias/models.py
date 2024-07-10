a7eed95009ed55211badf9bc609271b48c5a51ae
gemaquejr <gemaquejr@hotmail.com>
2024-07-09 23:34:50 -0300

    Adiciona relacionamento entre atleta e categoria
    Signed-off-by: gemaquejr <gemaquejr@hotmail.com>

--- crossfit_api/categorias/models.py
+++ crossfit_api/categorias/models.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Integer, String
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from crossfit_api.contrib.models import BaseModel
 
 
@@ -8,3 +8,4 @@ class CategoriaModel(BaseModel):
 
     pk_id: Mapped[int] = mapped_column(Integer, primary_key=True)
     nome: Mapped[str] = mapped_column(String(50), nullable=False)
+    categoria: Mapped["AtletaModel"] = relationship(back_populates="categoria")
