6594dd0a2c3b1a777350a4cf2480c357476116fd
gemaquejr <gemaquejr@hotmail.com>
2024-07-09 23:36:16 -0300

    Adiciona diretorio centro de treinamento
    Signed-off-by: gemaquejr <gemaquejr@hotmail.com>

---
+++ crossfit_api/centro_treinamento/schemas.py
@@ -0,0 +1,10 @@
+from typing import Annotated
+
+from pydantic import Field
+from crossfit_api.contrib.schemas import BaseSchema
+
+
+class CentroTreinamento(BaseSchema):
+    nome: Annotated[str, Field(description="Nome do centro de treinamento", example="CT Fit", max_length=20)]
+    endereco: Annotated[str, Field(description="Endereço do centro de treinamento", example="Rua A, 123", max_length=60)]
+    proprietario: Annotated[str, Field(description="Proprietário do centro de treinamento", example="Gemaque", max_length=30)]
