8dfa2e96d816caead68aafc8731620eaaaf7f732
gemaquejr <gemaquejr@hotmail.com>
2024-07-09 21:13:49 -0300

    Fix: schemas.py de atleta
    Signed-off-by: gemaquejr <gemaquejr@hotmail.com>

--- crossfit_api/atleta/schemas.py
+++ crossfit_api/atleta/schemas.py
@@ -1,8 +1,10 @@
 from typing import Annotated
-from pydantic import BaseModel, Field, PositiveFloat
+from pydantic import Field, PositiveFloat
+
+from crossfit_api.contrib.schemas import BaseSchema
 
 
-class Atleta(BaseModel):
+class Atleta(BaseSchema):
     nome: Annotated[str, Field(description="Nome do atleta", examples="João", max_length=50)]
     cpf: Annotated[str, Field(description="CPF do atleta", examples="12345678910", max_length=11)]
     idade: Annotated[int, Field(description="Idade do atleta", examples=25)]
