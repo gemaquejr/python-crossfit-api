9cb8c18d2165e06dbdf2ba503892ba1e642ca929
gemaquejr <gemaquejr@hotmail.com>
2024-07-10 18:32:24 -0300

    Fix: dependencies.py
    Signed-off-by: gemaquejr <gemaquejr@hotmail.com>

--- crossfit_api/contrib/dependencies.py
+++ crossfit_api/contrib/dependencies.py
@@ -3,6 +3,6 @@ from fastapi import Depends
 
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from crossfit_api.configs.database import get_db
+from crossfit_api.configs.database import get_session
 
-DatabaseDependency = Annotated[AsyncSession, Depends(get_db)]
+DatabaseDependency = Annotated[AsyncSession, Depends(get_session)]
