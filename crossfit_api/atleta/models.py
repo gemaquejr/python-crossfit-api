d2c5baa7256a2c6469b33cb5b0b07a5f1cef79eb
gemaquejr <gemaquejr@hotmail.com>
2024-07-11 18:18:02 -0300

    Fix: Refatora models.py
    Signed-off-by: gemaquejr <gemaquejr@hotmail.com>

--- crossfit_api/atleta/models.py
+++ crossfit_api/atleta/models.py
@@ -1,11 +1,11 @@
 from datetime import datetime
-from sqlalchemy import Float, Integer, String, DateTime, ForeignKey
+from sqlalchemy import DateTime, ForeignKey, Integer, String, Float
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from crossfit_api.contrib.models import BaseModel
 
 
 class AtletaModel(BaseModel):
-    __tablename__ = "atletas"
+    __tablename__ = 'atletas'
 
     pk_id: Mapped[int] = mapped_column(Integer, primary_key=True)
     nome: Mapped[str] = mapped_column(String(50), nullable=False)
@@ -15,7 +15,7 @@ class AtletaModel(BaseModel):
     altura: Mapped[float] = mapped_column(Float, nullable=False)
     sexo: Mapped[str] = mapped_column(String(1), nullable=False)
     created_at: Mapped[datetime] = mapped_column(DateTime, nullable=False)
-    categoria: Mapped["CategoriaModel"] = relationship(back_populates="atleta")
+    categoria: Mapped['CategoriaModel'] = relationship(back_populates="atleta", lazy='selectin')
     categoria_id: Mapped[int] = mapped_column(ForeignKey("categorias.pk_id"))
-    centro_treinamento: Mapped["CentroTreinamentoModel"] = relationship(back_populates="atleta")
+    centro_treinamento: Mapped['CentroTreinamentoModel'] = relationship(back_populates="atleta", lazy='selectin')
     centro_treinamento_id: Mapped[int] = mapped_column(ForeignKey("centros_treinamento.pk_id"))
